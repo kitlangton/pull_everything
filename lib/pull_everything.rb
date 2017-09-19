9400e6024a0596a9656c120ab20b0ffd6866a224
Kit Langton <kitlangton@gmail.com>
2017-09-19 12:24:02 -0400

    finish

--- lib/pull_everything.rb
+++ lib/pull_everything.rb
@@ -1,7 +1,11 @@
 require "pull_everything/version"
+require "octokit"
 
 module PullEverything
-  def run
+  def self.run
+    puts "Let's make Pull Requests for All The Things!"
+    puts "----------"
+
     puts "What's your GitHub username?"
     username = gets.chomp
     puts "What's your GitHub password?"
@@ -12,6 +16,25 @@ module PullEverything
       c.password = password
     end
 
-    p Octokit.user
+    Octokit.auto_paginate = true
+    forks = Octokit.repos(nil, type: 'owner')
+    forks.each do |fork|
+      next unless fork.fork && fork.owner.login == username
+      repo = Octokit.repo(fork.full_name)
+      sleep 1
+      next unless repo&.parent&.owner&.login == 'vikingeducation'
+      parent = repo.parent.full_name
+      create_pr(parent, username)
+    end
+  end
+
+  def self.create_pr(repo, username)
+    begin
+      Octokit.create_pull_request(repo, "master", "#{username}:master", "#{username}'s Solution'")
+    rescue Octokit::UnprocessableEntity
+      puts "#{repo}: PR already exists."
+      return
+    end
+    puts "#{repo}: Created PR!"
   end
 end
